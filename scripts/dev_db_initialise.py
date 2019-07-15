6ea8e4cd8b548bd769ced935871e28f448c9dee9
jordan bonser <jordan_bonser@live.co.uk>
2019-07-15 23:35:49 +0100

    Added a simple script that inputs default db data.
    It adds some basic users, teams and team_members.
    This is simply to allow more rapid development of
    the frontend app.

---
+++ scripts/dev_db_initialise.py
@@ -0,0 +1,17 @@
+from app import create_app
+from app.teams.service import create_team_in_db
+from app.users.service import create_user_in_db
+from app.team_members.service import create_team_member_in_db
+from app.team_roles.service import get_team_role_by_name
+
+app = create_app("dev")
+app.app_context().push()
+
+admin_user = create_user_in_db("admin", "password")
+member_user = create_user_in_db("member", "password")
+team = create_team_in_db("default_team")
+member_role = get_team_role_by_name("Member")
+admin_role = get_team_role_by_name("Admin")
+
+create_team_member_in_db(team, member_user, member_role)
+create_team_member_in_db(team, admin_user, admin_role)
